00419c6b96f7e891964dfa63b819d5efeeb7c439
N0wtty <adriankaiky1346@gmail.com>
2024-10-28 16:16:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const button = document.getElementById("jackpotbutton");
+const message = document.getElementById("message");
+
+button.addEventListener("click", function () {
+   message.textContent="jackpot!";
+
+});
+
+
+import
